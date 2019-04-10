41e28abd936468800d17faf9a398f98d4c890cdd
lingji0318 <zhaoyingqi@thextrader.cn>
2019-04-10 11:51:02 +0800

    gitstash 之后

--- src/animal.java
+++ src/animal.java
@@ -1,6 +1,8 @@
 interface animal {
     public void eat();
     public void travel();
-   public void zhengzaixiededaima();
-   public void bug();
+    public void zhengzaixiededaima();
+    public void bug();
+    public void test_stash();
+    public void stashzhihoudaima();
 }
